4a871e9bcfe1a67aeaf4d134171d7cb6265ddf64
L-CostaGabe <gabriecosta04lima@gmail.com>
2025-07-08 20:43:43 -0300

    contador

---
+++ src/controle_fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package controle_fluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("Parâmetros inválidos");
+    }
+}
